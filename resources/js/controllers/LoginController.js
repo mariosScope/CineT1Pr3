3d42d719dd9b252a9e883a0a79007ce0be852ba6
Chris Esquivel <ceskiv@gmail.com>
2015-05-17 01:08:40 -0600

    CE: Mejor validacion de login (DEV-1)

--- resources/js/controllers/LoginController.js
+++ resources/js/controllers/LoginController.js
@@ -6,10 +6,12 @@
  */
 
 var LoginController = function($scope, $location) {
-	$scope.validateForm = function() {
-		if (!angular.isUndefined($scope.userForm.email) && $scope.userForm.password.length > 0 ) {
- 			console.log($scope.userForm.email + ' | ' + $scope.userForm.password);
+    $scope.validateForm = function() {
+
+        if (!angular.isUndefined($scope.email.text) && $scope.password.text.length > 0) {
+            console.log($scope.email.text + ' | ( ͡° ͜ʖ ͡°) | ' + $scope.password.text);
             window.location.href = "seat.html";
-		}
-	};
+        }
+
+    };
 }
